f2a5d1ac38cabc036884f845b683572d326d5b7c
Yelk11 <neighbour.matthew@gmail.com>
2021-11-29 11:50:09 -0500

    update

---
+++ CountVowels/src/countVowels.c
@@ -0,0 +1,30 @@
+
+bool isVowel(char);
+
+
+
+int main(){
+    char vowels[] = {'a','e','i','o','u','y'};
+    char str[] = "Hello, World!";
+    int str_count = 0;
+    int vowel_count = 0;
+
+    while(str[str_count] != '\0')
+    {
+        
+        str_count++;
+    }
+
+
+}
+
+
+bool isVowel(char c){
+    
+
+}
+
+
+
+
+
